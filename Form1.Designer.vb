e9f197fae5e86406fc9f50b69f5281d08b3299c6
rajadirantobarasa0609 <rajadirantobarasa0609@gmail.com>
2025-02-23 15:03:14 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnBacaFile = New System.Windows.Forms.Button()
+        Me.btnCariKata = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.txtCari = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(218, 90)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(148, 87)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Name"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(477, 90)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(132, 87)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Text"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'btnBacaFile
+        '
+        Me.btnBacaFile.Location = New System.Drawing.Point(218, 212)
+        Me.btnBacaFile.Name = "btnBacaFile"
+        Me.btnBacaFile.Size = New System.Drawing.Size(148, 55)
+        Me.btnBacaFile.TabIndex = 2
+        Me.btnBacaFile.Text = "Baca File"
+        Me.btnBacaFile.UseVisualStyleBackColor = True
+        '
+        'btnCariKata
+        '
+        Me.btnCariKata.Location = New System.Drawing.Point(477, 212)
+        Me.btnCariKata.Name = "btnCariKata"
+        Me.btnCariKata.Size = New System.Drawing.Size(132, 55)
+        Me.btnCariKata.TabIndex = 3
+        Me.btnCariKata.Text = "Cari Kata"
+        Me.btnCariKata.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(218, 301)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(148, 26)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.Text = "Name"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(477, 301)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(132, 26)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.Text = "Properties"
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(218, 365)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtOutput.Size = New System.Drawing.Size(148, 26)
+        Me.txtOutput.TabIndex = 6
+        Me.txtOutput.Text = "Output"
+        '
+        'txtCari
+        '
+        Me.txtCari.Location = New System.Drawing.Point(477, 365)
+        Me.txtCari.Name = "txtCari"
+        Me.txtCari.Size = New System.Drawing.Size(132, 26)
+        Me.txtCari.TabIndex = 7
+        Me.txtCari.Text = "Masukkan Kata"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 534)
+        Me.Controls.Add(Me.txtCari)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnCariKata)
+        Me.Controls.Add(Me.btnBacaFile)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents btnBacaFile As Button
+    Friend WithEvents btnCariKata As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents txtCari As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports System.IO
+
+Public Class Form1
+    ' Tombol untuk memilih dan membaca file
+    Private Sub btnBacaFile_Click(sender As Object, e As EventArgs) Handles btnBacaFile.Click
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim filePath As String = openFileDialog.FileName
+            BacaFile(filePath)
+        End If
+    End Sub
+
+    ' Prosedur untuk membaca file
+    Private Sub BacaFile(ByVal path As String)
+        If File.Exists(path) Then
+            Dim isiFile As String = File.ReadAllText(path)
+            txtOutput.Text = isiFile ' Menampilkan isi file ke dalam TextBox
+        Else
+            MessageBox.Show("File tidak ditemukan!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    ' Tombol untuk mencari kata dalam file
+    Private Sub btnCariKata_Click(sender As Object, e As EventArgs) Handles btnCariKata.Click
+        Dim kataDicari As String = txtCari.Text.Trim()
+        Dim isiFile As String = txtOutput.Text
+
+        If String.IsNullOrEmpty(isiFile) Then
+            MessageBox.Show("Silakan baca file terlebih dahulu.", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Memeriksa apakah kata ditemukan dalam file
+        Dim ditemukan As Boolean = False
+        Dim barisArray() As String = isiFile.Split(vbCrLf)
+
+        ' Perulangan untuk mencari kata
+        For Each baris As String In barisArray
+            If baris.Contains(kataDicari) Then
+                ditemukan = True
+                Exit For
+            End If
+        Next
+
+        ' Percabangan untuk menampilkan hasil pencarian
+        If ditemukan Then
+            MessageBox.Show("Kata '" & kataDicari & "' ditemukan dalam file.", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Kata '" & kataDicari & "' tidak ditemukan dalam file.", "Hasil Pencarian", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+End Class
